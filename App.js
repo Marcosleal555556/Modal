fcfa4db2f795b579a8bbd4b23fba13332b95b44b
Marcosleal555556 <marcosantoniocordeiro27@gmail.com>
2024-03-22 14:14:42 -0300

    up

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Text, View, StyleSheet, Button, Modal } from "react-native";
 
 export default function App() {
+  const [visible, setVisible] = useState(false);
+
+  visModal = (vis) => {
+    if (!visible) {
+      setVisible(vis);
+    } else {
+      setVisible(vis);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title="Abrir modal" onPress={() => visModal(true)} />
+      {/* true or false*/}
+      <Modal transparent={true} animationType="fade" visible={visible}>
+        <View style={styles.fundo}>
+        <View style={styles.modal}>
+        <Text style={{ fontSize: 200 }}>Marcos</Text>
+        <Button title="Fechar modal" onPress={() => visModal(false)} />
+        </View>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -13,8 +31,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  fundo: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  modal: {
+    backgroundColor: "orange",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "80%",
+    height: "80%",
+    fontsize: 20,
   },
 });
